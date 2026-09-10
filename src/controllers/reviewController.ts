5ce8067de35e06c5f03168fd0ac6a5efcc35f8d9
Chariklia <xarakalpa@gmail.com>
2026-09-10 11:16:01 +0200

    fix: add validation to createReview

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -1,6 +1,7 @@
 // Ansvarsområde 3 - Chariklia
 import { Request, Response } from "express";
 import Review from "../models/review";
+import { Book } from "../models/Book";
 
 export const getAllReviews = async (req: Request, res: Response) => {
   try {
@@ -27,10 +28,19 @@ export const getReviewById = async (req: Request, res: Response) => {
 export const createReview = async (req: Request, res: Response) => {
   const { name, content, rating, book_id } = req.body;
   if (!name || !content || !rating || !book_id) {
-    res.status(400).json({ message: "name, content, rating and review_id are required" });
+    res.status(400).json({ message: "name, content, rating and book_id are required" });
+    return;
+  }
+  if (rating <1 || rating >5) {
+    res.status(400).json({ message: "rating must be between 1 and 5" });
     return;
   }
   try {
+    const bookExists = await Book.findById(book_id);
+    if (!bookExists) {
+      res.status(404).json({ message: "Book not found" });
+      return;
+    }
     const review = await Review.create({ name, content, rating, book_id });
     res.status(201).json(review);
   } catch (e) {
@@ -42,7 +52,7 @@ export const updateReview = async (req: Request, res: Response) => {
   try {
     const review = await Review.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!review) {
-      res.status(404).json({ message: "Review not found "});
+      res.status(404).json({ message: "Review not found" });
       return;
     }
     res.json(review);
